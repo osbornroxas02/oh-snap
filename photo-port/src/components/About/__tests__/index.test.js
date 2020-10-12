4dee67a9bd4085f78413be4573d4e6d5107d8d1f
osbornroxas <osbornroxas02@gmail.com>
2020-10-12 05:03:15 -0700

    tests for about & Nav

--- photo-port/src/components/About/__tests__/index.test.js
+++ photo-port/src/components/About/__tests__/index.test.js
@@ -3,6 +3,8 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
+afterEach(cleanup);
+
 describe('About component', () => {
     // First Test
     it('renders', () => {
@@ -11,9 +13,10 @@ describe('About component', () => {
 
     // Second Test
     it('matches snapshot DOM node structure', () => {
-        // render About
         const { asFragment } = render(<About />);
         expect(asFragment()).toMatchSnapshot();
 
     });
 })
+
+
