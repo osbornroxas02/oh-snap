151cf3b02e4632e10483a3ebc9fef98b26d79892
osbornroxas <osbornroxas02@gmail.com>
2020-10-13 02:59:23 -0700

    add conditional styling

--- photo-port/src/components/Nav/index.js
+++ photo-port/src/components/Nav/index.js
@@ -7,11 +7,13 @@ function Nav(props) {
         categories = [],
         setCurrentCategory,
         currentCategory,
+        contactSelected,
+        setContactSelected
     } = props;
 
-    // useEffect(() => {
-    //     document.title = capitalizeFirstLetter(currentCategory.name);
-    // }, [currentCategory]);
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);
 
     return (
         < header className="flex-row px-1" >
@@ -24,12 +26,12 @@ function Nav(props) {
             <nav>
                 <ul className="flex-row">
                     <li className="mx-2">
-                        <a data-testid="about" href="#about">
+                        <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
                             About me
                         </a>
                     </li>
-                    <li className="mx-2">
-                        <span>Contact</span>
+                    <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+                    <span onClick={() => setContactSelected(true)}>Contact</span>
                     </li>
                     {categories.map((category) => (
                         <li
@@ -38,10 +40,10 @@ function Nav(props) {
                                 }`}
                             key={category.name}
                         >
-                            <span
-                                onClick={() => {
-                                    setCurrentCategory(category)
-                                }}
+                            <span onClick={() => {
+                                setCurrentCategory(category);
+                                setContactSelected(false);
+                            }}
                             >
                                 {capitalizeFirstLetter(category.name)}
                             </span>
